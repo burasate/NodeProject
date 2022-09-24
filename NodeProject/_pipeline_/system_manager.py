b222d75061f408377299bed3b8cc545ae077394f
burasate <61697687+burasate@users.noreply.github.com>
2022-09-24 21:18:31 +0700

    0

--- NodeProject/_pipeline_/system_manager.py
+++ NodeProject/_pipeline_/system_manager.py
@@ -27,7 +27,7 @@ pd.set_option('display.width', None)
 from gSheet import gSheet
 gSheet.sheetName = 'Node Project Integration'
 from notionDatabase import notionDatabase as ntdb
-from gDrive import gDrive
+#from gDrive import gDrive
 from maReader import maReader
 
 # Path
