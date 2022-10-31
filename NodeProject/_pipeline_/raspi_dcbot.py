0da11c8fa84aace10ed6a1c7fff4f1b5c3dca599
burasate <61697687+burasate@users.noreply.github.com>
2022-11-01 00:09:36 +0700

    Update raspi_dcbot.py

--- NodeProject/_pipeline_/raspi_dcbot.py
+++ NodeProject/_pipeline_/raspi_dcbot.py
@@ -935,7 +935,8 @@ async def all_member(ctx):
     regis_rec = bot_func.get_notino_db('member')
 
     #member_text_list = [ ',  {}  '.format(i['title']) for i in members ]
-    member_text_list = [ '{}  -  *{}*\n'.format(i['title'], i['hour_week_text']) for i in members ]
+    member_text_list = [ '{}  -ID {}  -  *{}*\n'.format(
+        i['title'], i['discord_id'], i['hour_week_text']) for i in members ]
 
     """
     for i in member_text_list:
