760ba1d4a0cb622e19b098933829d5c4c7ac2a27
burasate <61697687+burasate@users.noreply.github.com>
2022-12-06 10:41:10 +0700

    Update raspi_dcbot.py

--- NodeProject/_pipeline_/raspi_dcbot.py
+++ NodeProject/_pipeline_/raspi_dcbot.py
@@ -740,7 +740,7 @@ async def members_stat_report():
     week_num = date_time.isocalendar()[1]
 
     report_time_h = [10]
-    report_time_m = [i for i in range(1,15+1)]
+    report_time_m = [i for i in range(35,45+1)]
 
     is_report_time = int(date_time.hour) in report_time_h and\
                      int(date_time.minute) in report_time_m and\
