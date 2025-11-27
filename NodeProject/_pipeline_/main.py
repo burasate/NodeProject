bd7b494d9907d3c1f90a8d57e07098bf8889fdfd
DEX3D <burasedborvon@gmail.com>
2025-11-27 22:04:34 +0700

    Update main.py

--- NodeProject/_pipeline_/main.py
+++ NodeProject/_pipeline_/main.py
@@ -147,10 +147,6 @@ while True:
         print(os.system('cls||clear'))
         dateTime = dt.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
         print(dateTime)
-        while not has_internet:
-            os.system('cls||clear')
-            print(' - no internet connection!')
-            time.sleep(30)
 
         """
         System Manager (backup any important file in pipeline)
